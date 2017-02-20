3599262efccc823c249b7ff724963500e31f8a4c
Niall Cooling <niall.cooling@gmail.com>
2017-02-20 17:30:06 +0000

    fully working simple fixed block allocator

--- sfba.h
+++ sfba.h
@@ -1,60 +1,84 @@
 #pragma once
-#include <cstdint>
 #include <array>
 
-
-template <typename T, std::uint32_t heap_size>
+template <typename T, unsigned pool_size>
 class sfba // Simple Fixed Block Allocator
 {
 public:
     using elem = T;
     sfba() = default;
-    std::uint32_t capacity() const;
-    std::uint32_t size() const;
-    T* malloc();
+    unsigned capacity() const;
+    unsigned size() const;
+    T* malloc(unsigned);
     void free(T* p);
 private:
-    std::array<bool,heap_size> available {};
-    std::array<elem,heap_size> heap {};
+    std::array<bool,pool_size> allocated {};
+    std::array<elem,pool_size> pool {};
+    std::array<unsigned,pool_size> allocCount {};
+
+    template <unsigned num>
+    T* alloc()
+    {
+        T* rv = nullptr;
+        std::array<bool,num> request{}; // set up array for search pattern 
+        // see if we can find subgroup in availabe blocks
+        auto it = std::search(begin(allocated),end(allocated),begin(request),end(request));
+        if(it != end(allocated)) {
+            unsigned element = (it - begin(allocated));  // calcualate pos of first free group
+            for (unsigned cnt = 0; cnt < num; ++cnt) {
+                allocated[element+cnt] = true;  // set each block to allocated
+            }
+            allocCount[element] = num; // remember how many blocks we allocated
+            rv = &pool[element];      // address of block
+        }
+        return rv;
+    }
 };
 
 
-template <typename T, std::uint32_t heap_size>
-std::uint32_t sfba<T,heap_size>::capacity() const
+template <typename T, unsigned pool_size>
+unsigned sfba<T,pool_size>::capacity() const
 {
-    return std::count_if(begin(available), end(available), [](bool b){ return b == false;});
+    return std::count_if(begin(allocated), end(allocated), [](bool b){ return b == false;});
 }
 
-template <typename T, std::uint32_t heap_size>
-std::uint32_t sfba<T,heap_size>::size() const
+template <typename T, unsigned pool_size>
+unsigned sfba<T,pool_size>::size() const
 {
-    return heap_size;
+    return pool_size;
 }
 
 
-template <typename T, std::uint32_t heap_size>
-T* sfba<T,heap_size>::malloc()
+template <typename T, unsigned pool_size>
+T* sfba<T,pool_size>::malloc(unsigned num)
 {
-    uint32_t element{};
-    for(auto i : available) {
-        if (i == false) {
-            available[element] = true;  // set block to unavaialble
-            return &heap[element];      // return address of block
-        }
-        ++element;
+    switch(num) {
+        case  1: return sfba<T, pool_size>::alloc<1>();
+        case  2: return sfba<T, pool_size>::alloc<2>();
+        case  4: return sfba<T, pool_size>::alloc<4>();
+        case  8: return sfba<T, pool_size>::alloc<8>();
+        case 16: return sfba<T, pool_size>::alloc<16>();
+        case 32: return sfba<T, pool_size>::alloc<32>();
+        case 64: return sfba<T, pool_size>::alloc<64>();
     }
     return nullptr;
 }
 
-template <typename T, std::uint32_t heap_size>
-void sfba<T,heap_size>::free(T* p)
+template <typename T, unsigned pool_size>
+void sfba<T,pool_size>::free(T* p)
 {
-    uint32_t element{};
-    for(auto i : heap) {
-        if (p == &heap[element]) {   // see if ptr address matches a block
-            available[element] = false;     // make block avaialble 
-            heap[element] = T{};            // clear the contents
+    unsigned element{};
+    for(auto i : pool) {
+        if (!(p == &pool[element])) {   // see if ptr address matches a block
+             ++element;
         }
-        ++element;
+        else {
+            for(unsigned cnt = 0; cnt < allocCount[element]; ++cnt) {
+                allocated[element+cnt] = false;     // make block avaialble 
+                pool[element+cnt] = T{};            // clear the contents
+            }
+            allocCount[element] = 0;
+            return;
+        }
     }
 }
--- tests.cpp
+++ tests.cpp
@@ -60,16 +60,61 @@ namespace
 
   TEST_F(SFBA_Test, initialAllocation)
   {
-    ASSERT_NE(nullptr, freeStore.malloc());
+    ASSERT_NE(nullptr, freeStore.malloc(1));
     ASSERT_EQ(heap_size-1, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, allocate2)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(2));
+    ASSERT_EQ(heap_size-2, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, allocate4)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(4));
+    ASSERT_EQ(heap_size-4, freeStore.capacity());
+  }
+
+
+  TEST_F(SFBA_Test, allocate8)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(8));
+    ASSERT_EQ(heap_size-8, freeStore.capacity());
+  }
+
+
+  TEST_F(SFBA_Test, allocate16)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(16));
+    ASSERT_EQ(heap_size-16, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, allocate32)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(32));
+    ASSERT_EQ(heap_size-32, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, failToAllocate64)
+  {
+    ASSERT_EQ(nullptr, freeStore.malloc(64));
+  }
+
+  TEST_F(SFBA_Test, initialAllocationNum2x2)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(2));
+    ASSERT_EQ(heap_size-2, freeStore.capacity());
+    ASSERT_NE(nullptr, freeStore.malloc(2));
+    ASSERT_EQ(heap_size-4, freeStore.capacity());
   }
 
   TEST_F(SFBA_Test, twoAllocationsAreNotEqual)
   {
-    elem_ty* ptr1 = freeStore.malloc();
+    elem_ty* ptr1 = freeStore.malloc(1);
     ASSERT_NE(nullptr, ptr1);
 
-    elem_ty* ptr2 = freeStore.malloc();
+    elem_ty* ptr2 = freeStore.malloc(1);
     ASSERT_NE(nullptr, ptr2);
 
 
@@ -77,27 +122,81 @@ namespace
     ASSERT_EQ(heap_size-2, freeStore.capacity());
   }
 
+  TEST_F(SFBA_Test, twoNumAllocationsAreNotEqual)
+  {
+    elem_ty* ptr1 = freeStore.malloc(2);
+    ASSERT_NE(nullptr, ptr1);
+
+    elem_ty* ptr2 = freeStore.malloc(2);
+    ASSERT_NE(nullptr, ptr2);
+
+
+    ASSERT_NE(ptr1, ptr2);
+    ASSERT_EQ(heap_size-4, freeStore.capacity());
+  }
+
   TEST_F(SFBA_Test, exhaustAllocation)
   {
     for(uint32_t counter = 0; counter < freeStore.size(); ++counter) {
       ASSERT_EQ(freeStore.size()-counter, freeStore.capacity());
-      ASSERT_NE(nullptr, freeStore.malloc());
+      ASSERT_NE(nullptr, freeStore.malloc(1));
     }
     ASSERT_EQ(0, freeStore.capacity());
-    ASSERT_EQ(nullptr, freeStore.malloc());
+    ASSERT_EQ(nullptr, freeStore.malloc(1));
+  }
+
+  TEST_F(SFBA_Test, exhaustAllocationNum32)
+  {
+    ASSERT_NE(nullptr, freeStore.malloc(32));
+    ASSERT_EQ(0, freeStore.capacity());
+    ASSERT_EQ(nullptr, freeStore.malloc(1));
   }
 
   TEST_F(SFBA_Test, initialDeallocation)
   {
-    elem_ty* ptr = freeStore.malloc();
+    elem_ty* ptr = freeStore.malloc(1);
     ASSERT_NE(nullptr, ptr);
     freeStore.free(ptr);
     ASSERT_EQ(heap_size, freeStore.capacity());
   }
 
+  TEST_F(SFBA_Test, deallocate2)
+  {
+    elem_ty* ptr = freeStore.malloc(2);
+    ASSERT_NE(nullptr, ptr);
+    freeStore.free(ptr);
+    ASSERT_EQ(heap_size, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, checkForOutOfOrderDealloc)
+  {
+    elem_ty* ptr1 = freeStore.malloc(1);
+    ASSERT_NE(nullptr, ptr1);
+    elem_ty* ptr2 = freeStore.malloc(1);
+    ASSERT_NE(nullptr, ptr2);
+    elem_ty* ptr3 = freeStore.malloc(1);
+    ASSERT_NE(nullptr, ptr3);
+
+    freeStore.free(ptr2);
+    ASSERT_EQ(heap_size-2, freeStore.capacity());
+  }
+
+  TEST_F(SFBA_Test, checkForOutOfOrderDeallocMisc)
+  {
+    elem_ty* ptr1 = freeStore.malloc(2);
+    ASSERT_NE(nullptr, ptr1);
+    elem_ty* ptr2 = freeStore.malloc(4);
+    ASSERT_NE(nullptr, ptr2);
+    elem_ty* ptr3 = freeStore.malloc(8);
+    ASSERT_NE(nullptr, ptr3);
+
+    freeStore.free(ptr2);
+    ASSERT_EQ(heap_size-10, freeStore.capacity());
+  }
+
   TEST_F(SFBA_Test, shouldNotDealloc)
   {
-    elem_ty* ptr = freeStore.malloc();
+    elem_ty* ptr = freeStore.malloc(1);
     elem_ty local{};
     freeStore.free(&local);
     ASSERT_EQ(heap_size-1, freeStore.capacity());
@@ -107,7 +206,7 @@ namespace
   {
     std::vector<elem_ty*> v;
     for(uint32_t counter = 0; counter < freeStore.size(); ++counter) {
-      v.push_back(freeStore.malloc());
+      v.push_back(freeStore.malloc(1));
     }
     ASSERT_EQ(0, freeStore.capacity());
 
